34475ceb24f4239d272854e63f4c0909c9c7b58c
Cedric <plouchard.cedric@gmail.com>
2020-04-12 19:27:44 +0200

    beer catalog service

---
+++ beer-catalog-service/src/test/java/com/example/beercatalog/BeerCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beercatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
